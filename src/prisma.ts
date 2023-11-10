ba4ff418970b3a4a7bcf52e9065fd9a7d334eaf1
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-10 09:49:34 +0100

    upd models

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,19 +1,21 @@
 import { PrismaClient, Role } from "@prisma/client";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient().$extends({
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient().$extends({
         model: {
             user: {
                 async signUp(email: string, password: string) {
-                    const hash = await bcrypt.hash(password, 10)
+                    const hash = await bcrypt.hash(password, 10);
                     return prisma.user.create({
                         data: {
                             username: email,
                             password: hash,
                         },
-                    },)
+                    });
                 },
             },
         },
