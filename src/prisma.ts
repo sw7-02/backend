624f96de4004645ebaadd85aa2d14f5ead547b2f
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-10 09:52:54 +0100

    mini prune

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Role } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
