0669ea0da65050b9d1b2ec63407a6346bdf85b44
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-14 10:11:45 +0100

    port

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ app.use(bodyParser.json());
 //Set all routes from routes folder
 app.use("/", routes);
 
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log("Server started on port 3000!");
 });
