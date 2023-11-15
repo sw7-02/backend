c1670e24fc841f353444dc1b2f88c7abf1106bc6
lasse2507a <44569984+lasse2507a@users.noreply.github.com>
2023-11-15 09:11:20 +0100

    Database (#16)
    * updated prisma schema + created seed file
    
    * updated readme with information on database seeding
    
    * updated seed file to contain mock data for all tables in schema
    
    * formatting
    
    ---------
    
    Co-authored-by: Thomas Lohse <thomas.k.lohse@gmail.com>

---
+++ prisma/seed.ts
@@ -0,0 +1,118 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+    // Create sample users
+    const user1 = await prisma.user.create({
+        data: {
+            username: "user1",
+            user_password: "password1",
+        },
+    });
+    const user2 = await prisma.user.create({
+        data: {
+            username: "user2",
+            user_password: "password2",
+        },
+    });
+
+    // Create sample courses
+    const course1 = await prisma.course.create({
+        data: {
+            title: "Course 1",
+        },
+    });
+    const course2 = await prisma.course.create({
+        data: {
+            title: "Course 2",
+        },
+    });
+
+    // Create sample sessions
+    const session1 = await prisma.session.create({
+        data: {
+            title: "Session 1",
+            course_id: course1.course_id,
+        },
+    });
+
+    // Create sample enrollments
+    await prisma.enrollment.create({
+        data: {
+            user_id: user1.user_id,
+            course_id: course1.course_id,
+            user_role: 0,
+            total_points: 5,
+        },
+    });
+
+    // Example for creating assignments:
+    const assignment1 = await prisma.assignment.create({
+        data: {
+            title: "Assignment 1",
+            description: "Description of Assignment 1",
+            code_template: "Your code template here",
+            due_date: new Date(),
+            course_id: course1.course_id,
+            programming_language: "JavaScript",
+        },
+    });
+    const assignmentSolution1 = await prisma.assignmentSolution.create({
+        data: {
+            assignment_id: assignment1.assignment_id,
+            user_id: user1.user_id,
+            solution: "Sample solution 1",
+            feedback: "Feedback for solution 1",
+        },
+    });
+
+    // Create sample exercises
+    const exercise1 = await prisma.exercise.create({
+        data: {
+            title: "Exercise 1",
+            description: "Description of Exercise 1",
+            points: 10,
+            programming_language: "JavaScript",
+            code_template: "Your code template here",
+            session_id: session1.session_id,
+        },
+    });
+
+    // Create sample test cases
+    const testCase1 = await prisma.testCase.create({
+        data: {
+            code: "Test case 1 code",
+            is_visible: true,
+            exercise_id: exercise1.exercise_id,
+        },
+    });
+
+    // Create sample hints
+    const hint1 = await prisma.hint.create({
+        data: {
+            description: "Hint 1 description",
+            order: 1,
+            exercise_id: exercise1.exercise_id,
+        },
+    });
+
+    // Create sample exercise solutions
+    const exerciseSolution1 = await prisma.exerciseSolution.create({
+        data: {
+            solution: "Sample exercise solution 1",
+            is_public: true,
+            is_pinned: false,
+            exercise_id: exercise1.exercise_id,
+            user_id: user1.user_id,
+        },
+    });
+
+    console.log("Sample data seeded successfully.");
+
+    await prisma.$disconnect();
+}
+
+seed().catch((error) => {
+    console.error("Error seeding data:", error);
+});
