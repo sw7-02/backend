f24e60433eb7bb35e4ad8a8fae53954fa3bf8eb0
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2023-11-15 07:55:21 +0100

    clean up

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,6 @@ const routes = Router();
 // enables passing json bodies.
 routes.use(Router.json());
 
-
 routes.use("/courses", course_overview);
 routes.use("/login", login);
 
@@ -16,16 +15,6 @@ routes.use("/login", login);
 // CRUD for Courses
 routes.route("/")
     .get(async (req: Request, res: Response) => {
-        try{
-            const courses = await prisma.course.findMany()
-            res.json(courses)
-        }catch (e) {
-            console.log(e.message)
-            res.status(500).json({
-                message: "Something went wrong",
-            })
-        }
-
         return res.send("u made a GET request");
     })
     .post((req: Request, res: Response) => {
@@ -38,62 +27,6 @@ routes.route("/")
         return res.send("u made a DELETE request");
     });
 
-
-// Get
-routes.get("/hey", (req: Request, res: Response) => {
-    return res.redirect("http://example.com");
-});
-
-// Put (Update)
-
-
-// Post
-routes.post("/api/data", (req: Request, res: Response) => {
-    console.log(req.body);
-    //res.redirect();
-    return res.sendStatus(200);
-});
-
-/*
-routes.post("/courses", async (req, res) => {
-    try {
-      const { name, games } = req.body
- 
-      //const newUser = await prisma.user.create({
-      //})
-    }
-});
-*/
-
-
-// Delete
-routes.delete('/course/:id', async (req, res) => {
-   const { id } = req.params;
-   const user = await prisma.user.delete({
-      where: {
-        id,
-        },
-      })
-      res.json(user)
-    //const projectIndex = routes.findIndex(p => p.id == id);
-   
-    //projects.splice(projectIndex, 1);
-   
-    return res.send();
-   });
-
-
-
-
-// test express operations
-
-/*
-routes.get("/", (_, res) => {
-    console.log("connect");
-
-    res.status(201).send("Hello World!");
-});
-*/
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
