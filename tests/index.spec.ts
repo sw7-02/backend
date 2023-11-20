7cda76e028d7dc349b1a35ddc16341cbce00e66e
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-20 11:21:18 +0100

    added test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,11 +1,29 @@
 import * as assert from "assert";
+import prisma from "../src/prisma";
 // how to test - https://thiagooliveirasantos.medium.com/typescript-unit-tests-best-practices-part-4-clean-test-suites-structure-94f5fd5fdf8
 
-describe("Testing framework test", () => {
+describe("Testing framework test (and prisma)", () => {
     it("add two numbers", () => {
         assert.equal(2 + 2, 4);
     });
     it("add two numbers", () => {
         assert.notEqual(2 + 3, 4);
+    });
+    it("prisma", async () => {
+        try {
+            return await prisma.user
+                .findFirstOrThrow({
+                    where: { username: "test", user_password: "encrypt" },
+                    select: { user_id: true, username: true },
+                })
+                .then(
+                    ({ user_id: userId, username }) =>
+                        console.log("what?"),
+                    (_) => {},
+                );
+        } catch (e) {
+            console.log("no errors");
+            assert.equal(true, true);
+        }
     });
 });
