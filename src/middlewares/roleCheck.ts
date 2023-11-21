d53115b21e21acebdf62aeb4ce6863321f5659d8
kspe20 <kspe20@student.aau.dk>
2023-11-21 10:57:13 +0100

    Made roleChecker middleware

---
+++ src/middlewares/roleCheck.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import prisma from "../prisma";
+
+export default function roleCheck (roles: number[]) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const userId = res.locals.jwtPayload.userId;
+        const courseId = parseInt(req.params.course_id);
+        const user = await prisma.enrollment.findFirstOrThrow({
+            where: {
+                course_id: courseId,
+                user_id: userId,
+            },
+            select: {
+                user_role: true,
+            },
+        });
+
+        if (roles.indexOf(user.user_role) > -1) {
+            next();
+        } else {
+            res.status(401).send();
+        }
+    };
+}
+
+    /*async function isTeacher(req: Request, res: Response, next: NextFunction) {
+        const userId = res.locals.jwtPayload.userId;
+        let user = await prisma.user.findUniqueOrThrow({
+            where: { user_id: userId },
+        });
+
+        if (user.is_teacher == true) {
+            next();
+        } else {
+            res.status(403).send();
+        }
+    }*/
