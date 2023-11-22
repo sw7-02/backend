a420ca32bcc7319dfafaf0c89f4a15cf4fb8834d
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-22 13:00:01 +0100

    new pass-strat

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -8,7 +8,10 @@ const specialCharRegEx = new RegExp(
     `[!@#$%^&*()]{${config.auth.pw.special_count}}`,
 );
 const numberRegEx = new RegExp(`([0-9].*){${config.auth.pw.num_count}}`);
-export async function validateAndHashPassword(pw: string, username: string): Promise<string> {
+export async function validateAndHashPassword(
+    pw: string,
+    username: string,
+): Promise<string> {
     const salt = bcrypt.hashSync(username, config.auth.salt);
     let pass = bcrypt.hash(pw, salt);
     if (pw.length < config.auth.pw.length)
@@ -35,74 +38,112 @@ export async function validateAndHashPassword(pw: string, username: string): Pro
     return await pass;
 }
 
+export function validatePassword(pw: string): Result<void> {
+    const code = 406;
+    if (pw.length < config.auth.pw.length)
+        return new Err(
+            code,
+            `Not long enough, should be at least ${
+                config.auth.pw.length
+            } character${config.auth.pw.length > 1 ? "s" : ""}`,
+        );
+
+    if (!numberRegEx.test(pw))
+        return new Err(
+            code,
+            `Not enough numbers supplied, there should be at least ${
+                config.auth.pw.num_count
+            } number${config.auth.pw.num_count > 1 ? "s" : ""}`,
+        );
+
+    if (!specialCharRegEx.test(pw))
+        return new Err(
+            code,
+            `Not enough special characters, there should be at least ${
+                config.auth.pw.special_count
+            } special character${config.auth.pw.special_count > 1 ? "s" : ""}`,
+        );
+}
+
+function genPass(pw: string, username: string): string {
+    const salt = bcrypt.hashSync(username, config.auth.salt);
+    return bcrypt.hashSync(pw, salt);
+}
+
 export default class AuthController {
     static login = async (
         username: string,
         password: string,
     ): Promise<Result<string>> => {
-        return await validateAndHashPassword(password, username).then(
-            async (encrypt) => {
-                return prisma.user
-                    .findUniqueOrThrow({
-                        where: { username },
-                        select: {
-                            user_id: true,
-                            username: true,
-                            user_password: true,
-                        },
-                    })
-                    .then(
-                        ({ user_id, username, user_password }) => {
-                            if (!bcrypt.compareSync(password, user_password)) {
-                                console.log(password);
-                                console.log(encrypt);
-                                console.log(user_password);
-                                console.error(
-                                    `Attempt login on user ${username} (wrong password)`,
-                                );
-                                return new Err(401, "Wrong password");
-                            }
-                            return generateJWTToken({
-                                userId: user_id,
-                                username,
-                            });
-                        },
-                        (e) => {
-                            console.error(
-                                `Fail logging in user ${username}: ${e}`,
-                            );
-                            return new Err(401, `Username does not exist`);
-                        },
-                    );
-            },
-            (e) => new Err(406, `Password not valid: ${e}`),
-        );
+        const valid = validatePassword(password);
+        if (valid instanceof Err)
+            return {
+                code: valid.code,
+                msg: `Password not valid: ${valid.msg}`,
+            };
+
+        return prisma.user
+            .findUniqueOrThrow({
+                where: { username },
+                select: {
+                    user_id: true,
+                    username: true,
+                    user_password: true,
+                },
+            })
+            .then(
+                ({ user_id, username, user_password }) => {
+                    if (
+                        !bcrypt.compareSync(
+                            genPass(password, username),
+                            user_password,
+                        )
+                    ) {
+                        console.log(password);
+                        console.log(genPass(password, username));
+                        console.log(user_password);
+                        console.error(
+                            `Attempt login on user ${username} (wrong password)`,
+                        );
+                        return new Err(401, "Wrong password");
+                    }
+                    return generateJWTToken({
+                        userId: user_id,
+                        username,
+                    });
+                },
+                (e) => {
+                    console.error(`Fail logging in user ${username}: ${e}`);
+                    return new Err(401, `Username does not exist`);
+                },
+            );
     };
 
     static signUp = async (
         username: string,
         password: string,
     ): Promise<Result<string>> => {
-        return await validateAndHashPassword(password, username).then(
-            async (encrypt) => {
-                return prisma.user
-                    .findFirstOrThrow({
-                        where: { username },
-                    })
-                    .then(
-                        () => new Err(409, "Username exists"),
-                        async () => {
-                            let { user_id: userId } = await prisma.user.create({
-                                data: {
-                                    username,
-                                    user_password: encrypt,
-                                },
-                            });
-                            return generateJWTToken({ userId, username });
+        const valid = validatePassword(password);
+        if (valid instanceof Err)
+            return {
+                code: valid.code,
+                msg: `Password not valid: ${valid.msg}`,
+            };
+        return prisma.user
+            .findFirstOrThrow({
+                where: { username },
+            })
+            .then(
+                () => new Err(409, "Username exists"),
+                async () => {
+                    let { user_id: userId } = await prisma.user.create({
+                        data: {
+                            username,
+                            user_password: genPass(password, username),
                         },
-                    );
-            },
-            (e) => new Err(406, `Password not valid: ${e}`),
-        );
+                    });
+                    return generateJWTToken({ userId, username });
+                },
+            );
     };
 }
