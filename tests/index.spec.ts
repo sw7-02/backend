27f555d4ec2f8520de2af0a9023f2571a14cbe65
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-24 10:52:46 +0100

    added test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,13 @@
 import * as assert from "assert";
 import prisma from "../src/prisma";
+import { before } from "mocha";
+import { seed } from "./lib/db";
 // how to test - https://thiagooliveirasantos.medium.com/typescript-unit-tests-best-practices-part-4-clean-test-suites-structure-94f5fd5fdf8
 
+before("Seed DB", async function() {
+    this.timeout(5000);
+    await seed();
+})
 describe("Testing framework test (and prisma)", () => {
     it("add two numbers", () => {
         assert.equal(2 + 2, 4);
