9810031b851b741c046621b6356e7cb96cd4e8ef
kspe20 <kspe20@student.aau.dk>
2023-11-24 10:53:36 +0100

    added seed to index.spec.ts

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import prisma from "../src/prisma";
+import { seed } from "./lib/db";
 // how to test - https://thiagooliveirasantos.medium.com/typescript-unit-tests-best-practices-part-4-clean-test-suites-structure-94f5fd5fdf8
 
 describe("Testing framework test (and prisma)", () => {
@@ -10,3 +11,8 @@ describe("Testing framework test (and prisma)", () => {
         assert.notEqual(2 + 3, 4);
     });
 });
+
+before("Seed DB", async function() {
+    this.timeout(5000);
+    await seed();
+})
