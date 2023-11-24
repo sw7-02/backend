04e758de2a2376c78f5f27c00021e3fbc285b4b8
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-24 10:56:37 +0100

    added test

--- tests/controllers/ExerciseController.spec.ts
+++ tests/controllers/ExerciseController.spec.ts
@@ -11,15 +11,15 @@ describe("ExerciseController testing", function () {
 
     it("Retrieve all exercises: Valid session", async function () {
         const result = await ExerciseController.retrieveAllExercises(1);
-        console.log(result);
         assert.notEqual(result instanceof Err, true);
         assert.equal((<[]>result).length, 1);
     });
     it("Retrieve all exercises: Invalid session", async function () {
         const result = await ExerciseController.retrieveAllExercises(1000);
-        console.log(result);
         assert.equal(result instanceof Err, true);
         assert.equal((<Err>result).code, 401);
         assert.equal((<Err>result).msg, "Session does not exist");
     });
+
+
 });
