9d2ea7110566c7a8c58b8cec98469c772319a8ad
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-24 13:21:46 +0100

    tests

--- tests/controllers/CourseController.spec.ts
+++ tests/controllers/CourseController.spec.ts
@@ -1,5 +1,4 @@
 import * as assert from "assert";
-import ExerciseController from "../../src/controllers/ExerciseController";
 import CourseController from "../../src/controllers/CourseController";
 import { Err } from "../../src/lib";
 
@@ -12,12 +11,35 @@ describe("ExerciseController testing", function () {
         assert.equal(res.sessions.length, 1);
         assert.equal(res.sessions[0].title, "Session 1");
         assert.equal(res.sessions[0].session_id, 1);
-
     });
     it("Retrieve course: Valid ID", async function () {
         const result = await CourseController.retrieveCourse(1000);
         assert.equal(result instanceof Err, true);
         assert.equal((<Err>result).code, 404);
-        assert.equal((<Err>result).msg, "Failed getting sessions: Invalid course ID");
+        assert.equal(
+            (<Err>result).msg,
+            "Failed getting sessions: Invalid course ID",
+        );
+    });
+    it("Retrieve full course: Valid ID", async function () {
+        const result = await CourseController.retrieveFullCourse(1);
+        assert.notEqual(result instanceof Err, true);
+        const res = <any>result;
+        assert.equal(res.title, "Course 1");
+        assert.equal(res.sessions.length, 1);
+        assert.equal(res.sessions[0].title, "Session 1");
+        assert.equal(res.sessions[0].session_id, 1);
+        assert.equal(res.sessions[0].exercises.length, 1);
+        assert.equal(res.sessions[0].exercises[0].title, "Exercise 1");
+        assert.equal(res.sessions[0].exercises[0].exercise_id, 1);
+    });
+    it("Retrieve full course: Valid ID", async function () {
+        const result = await CourseController.retrieveFullCourse(1000);
+        assert.equal(result instanceof Err, true);
+        assert.equal((<Err>result).code, 404);
+        assert.equal(
+            (<Err>result).msg,
+            "Failed getting sessions: Invalid course ID",
+        );
     });
 });
