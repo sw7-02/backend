019d163ea44421543f6e9471ce47f5a31c81f8f5
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-27 10:48:02 +0100

    fmt

--- src/routes/course/session/exercise.ts
+++ src/routes/course/session/exercise.ts
@@ -61,73 +61,81 @@ routes.post(
     },
 );
 
-routes.route("/:exercise_id").get(async (req: Request, res: Response) => {
-    const id: number = +req.params.exercise_id;
-    if (!id) {
-        res.status(400).send("ID not a number");
-        return;
-    }
-    const result = await ExerciseController.retrieveExercise(id);
-    if (result instanceof Err) {
-        const { code, msg } = result;
-        res.status(code).send(msg);
-    } else res.send(result);
-}).delete(
-    [roleCheck([Role.TEACHER])],
-    async (req: Request, res: Response) => {
+routes
+    .route("/:exercise_id")
+    .get(async (req: Request, res: Response) => {
         const id: number = +req.params.exercise_id;
         if (!id) {
             res.status(400).send("ID not a number");
             return;
         }
-        const result = await ExerciseController.deleteExercise(id);
+        const result = await ExerciseController.retrieveExercise(id);
         if (result instanceof Err) {
             const { code, msg } = result;
             res.status(code).send(msg);
-        } else res.send();
-    },
-).post(async (req: Request, res: Response) => {
-    const exerciseId: number = +req.params.exercise_id;
-    if (!exerciseId) {
-        res.status(400).send("ID not a number");
-        return;
-    }
+        } else res.send(result);
+    })
+    .delete(
+        [roleCheck([Role.TEACHER])],
+        async (req: Request, res: Response) => {
+            const id: number = +req.params.exercise_id;
+            if (!id) {
+                res.status(400).send("ID not a number");
+                return;
+            }
+            const result = await ExerciseController.deleteExercise(id);
+            if (result instanceof Err) {
+                const { code, msg } = result;
+                res.status(code).send(msg);
+            } else res.send();
+        },
+    )
+    .post(async (req: Request, res: Response) => {
+        const exerciseId: number = +req.params.exercise_id;
+        if (!exerciseId) {
+            res.status(400).send("ID not a number");
+            return;
+        }
 
-    // TODO: test the solution beforehand
+        // TODO: test the solution beforehand
 
-    const userId: number = res.locals.jwtPayload.userId;
-    const courseId = res.locals.courseId;
-    const { solution, is_anonymous } = req.body;
+        const userId: number = res.locals.jwtPayload.userId;
+        const courseId = res.locals.courseId;
+        const { solution, is_anonymous } = req.body;
 
-    let points;
-    const resultSubmission = await CourseController.updatePoints(
-        courseId,
-        userId,
-        exerciseId,
-    ).then((result) => {
-        if (!(result instanceof Err)) {
-            points = result;
-            return ExerciseController.submitExerciseSolution(
-                exerciseId,
-                userId,
-                solution,
-                is_anonymous,
-            );
-        } else return result;
+        let points;
+        const resultSubmission = await CourseController.updatePoints(
+            courseId,
+            userId,
+            exerciseId,
+        ).then((result) => {
+            if (!(result instanceof Err)) {
+                points = result;
+                return ExerciseController.submitExerciseSolution(
+                    exerciseId,
+                    userId,
+                    solution,
+                    is_anonymous,
+                );
+            } else return result;
+        });
+
+        if (resultSubmission instanceof Err) {
+            if (points)
+                // It found points, but failed in submitting => subtract the points
+                await CourseController.decrementPoints(
+                    courseId,
+                    userId,
+                    points,
+                );
+            const { code, msg } = resultSubmission;
+            res.status(code).send(msg);
+        } else res.send(resultSubmission);
     });
 
-    if (resultSubmission instanceof Err) {
-        if (points)
-            // It found points, but failed in submitting => subtract the points
-            await CourseController.decrementPoints(courseId, userId, points);
-        const { code, msg } = resultSubmission;
-        res.status(code).send(msg);
-    } else res.send(resultSubmission);
-});
-
 routes.post("/:exercise_id/test", (req: Request, res: Response) => {
-   // TODO: TEST CODE
-   res.send("Tested code (Unimplemented)");
+    // TODO: TEST CODE
+    res.send("Tested code (Unimplemented)");
 });
 
 // exercise solutions
