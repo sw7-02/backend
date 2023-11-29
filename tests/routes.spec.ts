98224aabced43fd5f560f7ced1fdde9a4b287223
kspe20 <kspe20@student.aau.dk>
2023-11-29 13:20:51 +0100

    testing supertest setup

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -4,12 +4,8 @@ const app = require("../src/index");
 
 describe("testing routes", function () {
     it("test route", async function (done) {
-        try {
-            const res = await supertest(app).get("/").end(); //.expect(200).end(done());
-            assert.equal(res.status, 200);
-        } catch (e) {
-            console.log(`THis is shit for this reason: ${e}`);
-        }
+        const res = await supertest(app).get("/").end(); //.expect(200).end(done());
+        assert.equal(res.status, 200);
         /*.then((response: { body: unknown }) => {
                 assert.equal(response.body, "u made a GET request");
             });*/
