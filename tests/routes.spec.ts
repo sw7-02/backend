6c720093e82e0bc0027741204c9b876973f01742
kspe20 <kspe20@student.aau.dk>
2023-11-29 12:26:44 +0100

    testing supertest setup

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -1,17 +1,12 @@
-import express from "express";
-import httpMocks from "node-mocks-http";
-import * as assert from "assert";
-
-const request = require("supertest");
-const app = express();
+const supertest = require("supertest");
+const assert = require("assert");
+const app = require("../src/index");
 
 describe("testing routes", function () {
     it("test route", function () {
-        return request(app)
-            .get("/status")
-            .expect(200)
-            .then((response: { body: unknown }) => {
-                assert.equal(response.body, "Service is running!");
-            });
+        return supertest(app).get("/").expect(200);
+        /*.then((response: { body: unknown }) => {
+                assert.equal(response.body, "u made a GET request");
+            });*/
     });
 });
