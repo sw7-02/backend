1b2df1148e97bc68f386e4a14fd6cb1d00ae40d9
kspe20 <kspe20@student.aau.dk>
2023-11-29 12:50:42 +0100

    testing supertest setup

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -3,6 +3,7 @@ const assert = require("assert");
 const app = require("../src/index");
 
 describe("testing routes", function () {
+
     it("test route", function (done) {
         return supertest(app).get("/").expect(200).end(done());
         /*.then((response: { body: unknown }) => {
