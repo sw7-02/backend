16c43af73d5948cda29621ff26a2b942f1af2f59
kspe20 <kspe20@student.aau.dk>
2023-11-30 08:59:18 +0100

    fmt

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -4,23 +4,21 @@ const app = require("../src/index");
 
 describe("testing routes", function () {
     it("test route", async function (done) {
-        let res = await supertest(app)
-            .get("/")
-            /*.end(function (err: any, res: Response) {
+        let res = await supertest(app).get("/");
+        /*.end(function (err: any, res: Response) {
                 if (err instanceof Error) done(err);
             }); //.expect(200).end(done());
         //assert.equal(res.status, 200);
         /*.then((response: { body: unknown }) => {
                 assert.equal(response.body, "u made a GET request");
             });*/
-        if (res instanceof Error){
-            console.log(`res == Error: ${res}`)
+        if (res instanceof Error) {
+            console.log(`res == Error: ${res}`);
             return done(res);
         }
-        console.log("right before done()")
+        console.log("right before done()");
         done();
-        console.log(`right before assert, ${res}`)
+        console.log(`right before assert, ${res}`);
         assert.equal(res.status, 200);
-
     });
 });
