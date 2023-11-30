7ca18668b05d875b2a0420ac2a185cfd37e53f72
kspe20 <kspe20@student.aau.dk>
2023-11-30 09:40:51 +0100

    testing supertest setup

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -1,9 +1,11 @@
-const supertest = require("supertest");
-const assert = require("assert");
+import supertest from "supertest"
+import assert from "assert"
+
+
 const app = require("../src/index");
 
 describe("testing routes", function () {
-    it("test route", async function () {
+    it("test route", async function (done) {
         this.timeout(10000);
         let res = await supertest(app).get("/");
         /*.end(function (err: any, res: Response) {
@@ -20,5 +22,6 @@ describe("testing routes", function () {
         console.log(`right before assert status, ${res.status}`);
         assert.equal(res.status, 200);
         console.log(`right after`);
+        done();
     });
 });
