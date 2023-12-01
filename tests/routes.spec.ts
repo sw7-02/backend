1ce57176ab822888904cf576ec02ba54f9b1d769
kspe20 <kspe20@student.aau.dk>
2023-12-01 09:06:47 +0100

    test setup chai

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -1,4 +1,4 @@
-/*import * as chai from "chai";
+import chai from "chai";
 import chaiHttp from "chai-http";
 import { app } from "../src/index";
 
@@ -13,4 +13,4 @@ describe("testing routes", function () {
             });
     });
 });
-*/
+
