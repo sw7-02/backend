e6feb0d26895a75c7bb12c3eacb9bcbfbbbbb36a
kspe20 <kspe20@student.aau.dk>
2023-12-01 10:53:00 +0100

    ci test

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -6,16 +6,13 @@ chai.use(chaiHttp);
 describe("testing routes", function () {
     const request = chai.request(app);
 
-    after((done) => {
-        request.close(), done();
-    });
-
     it("test route", (done) => {
         return chai
             .request(app)
             .get("/")
             .end((err, res) => {
                 chai.expect(res.status).to.eql(200);
+                request.close();
                 done();
         });
     });
