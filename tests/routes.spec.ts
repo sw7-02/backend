1e754d51f3460368bf0939947ac08588262a0a8f
kspe20 <kspe20@student.aau.dk>
2023-12-01 10:21:20 +0100

    CI test

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -1,9 +1,15 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
 import { app } from "../src/index";
+import http from "http";
 
 chai.use(chaiHttp);
 describe("testing routes", function () {
+    const server = http.createServer(app);
+    const request = chai.request(server);
+
+    after(done => server.close(done));
+
     it("test route", async function () {
         return chai
             .request(app)
