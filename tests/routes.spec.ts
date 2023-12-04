8e2f2cd3949687d1abb7772ae2e66e19612953b7
kspe20 <kspe20@student.aau.dk>
2023-12-04 10:08:32 +0100

    test

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -1,24 +1,19 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
 import { app } from "../src/index";
+import * as assert from "assert";
 
 chai.use(chaiHttp);
 describe("testing routes", function () {
     //const request = chai.request(app);
 
     it("test route", (done) => {
-        let server = chai.request(app);
-        server.close(() => done());
-        mocha.run(function (failures) {
-            process.exit(failures ? 1 : 0);
-        });
-        /*return chai
+        return chai
             .request(app)
             .get("/")
-            .end((err, res) => {
-                chai.expect(res.status).to.eql(200);
-                request.close();
-                return done();
-        });*/
+            .then((res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, "U made a GET request");
+            });
     });
 });
