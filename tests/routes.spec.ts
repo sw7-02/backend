70ff74cb2fed7f2ae08d8c4b1317fa88673cd7cd
kspe20 <kspe20@student.aau.dk>
2023-12-04 10:15:55 +0100

    simple get/post/put/delete request

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -7,13 +7,44 @@ chai.use(chaiHttp);
 describe("testing routes", function () {
     //const request = chai.request(app);
 
-    it("test route", async function () {
+    it("Simple GET request", async function () {
         return chai
             .request(app)
             .get("/")
             .then((res) => {
                 assert.equal(res.status, 200);
-                assert.equal(res.text, "U made a GET request");
+                assert.equal(res.text, "u made a GET request");
             });
     });
+
+    it("Simple POST request", async function () {
+        return chai
+            .request(app)
+            .post("/")
+            .then((res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, "u made a POST request");
+            });
+    });
+
+    it("Simple PUT request", async function () {
+        return chai
+            .request(app)
+            .put("/")
+            .then((res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, "u made a PUT request");
+            });
+    });
+
+    it("Simple DELETE request", async function () {
+        return chai
+            .request(app)
+            .delete("/")
+            .then((res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.text, "u made a DELETE request");
+            });
+    });
+
 });
